f3d18db419de1e31050e62fb1a868898c91638e5
TianBin <1627087576@qq.com>
2022-03-31 17:11:25 +0800

    idea first commit

---
+++ src/main/java/com/sdobiao/git/GitTest.java
@@ -0,0 +1,15 @@
+package com.sdobiao.git;
+
+/**
+ * ClassName: GitTest <br/>
+ * Description: <br/>
+ * date: 2022/3/31 17:05<br/>
+ *
+ * @author Administrator<br />
+ * @since JDK 1.8
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
